84e5a5cbe5bee82bf17cc285399c27ef48a151a2
Samuel Preston <samp.reston@outlook.com>
2022-09-23 15:57:41 +0100

    try new solutions to favicon issues

--- apps/blog/src/pages/_app.tsx
+++ apps/blog/src/pages/_app.tsx
@@ -3,10 +3,17 @@ import type { AppProps } from 'next/app'
 import React from 'react'
 
 import Layout from '../components/Layout'
+import Head from 'next/head'
 
 function MyApp ({ Component, pageProps }: AppProps): React.ReactNode {
   return (
     <Layout>
+      <Head>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
--- apps/blog/src/pages/_document.tsx
+++ apps/blog/src/pages/_document.tsx
@@ -6,7 +6,10 @@ class MyDocument extends Document {
     return (
       <Html className='scroll-smooth' lang='en'>
         <Head>
-          <link rel="shortcut icon" href="/static/favicon.ico" />
+          <link rel="shortcut icon" href="/images/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:ital,wght@0,300;0,400;0,500;0,700;1,200&display=swap" rel="stylesheet" />
