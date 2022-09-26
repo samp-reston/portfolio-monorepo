e581557dec624e9ada1f59ab4c39a595633bbad7
Samuel Preston <samp.reston@outlook.com>
2022-09-26 13:50:13 +0100

    try fix favicon

--- apps/portfolio/src/pages/_app.tsx
+++ apps/portfolio/src/pages/_app.tsx
@@ -7,15 +7,19 @@ import Head from 'next/head'
 
 function MyApp ({ Component, pageProps }: AppProps): React.ReactNode {
   return (
-    <Layout>
+    <>
       <Head>
         <link rel="shortcut icon" href="/images/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       </Head>
-      <Component {...pageProps} />
-    </Layout>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
