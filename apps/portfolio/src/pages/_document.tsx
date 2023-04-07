1172373774c96764e9143ec7b5d96b69d649f456
Samuel Preston <samp.reston@outlook.com>
2023-04-07 12:03:06 +0100

    Update _document.tsx

--- apps/portfolio/src/pages/_document.tsx
+++ apps/portfolio/src/pages/_document.tsx
@@ -7,7 +7,7 @@ class MyDocument extends Document {
       <Html className='scroll-smooth' lang='en'>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="use-credentials" />
           <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:ital,wght@0,300;0,400;0,500;0,700;1,200&display=swap" rel="stylesheet" />
         </Head>
         <body>
